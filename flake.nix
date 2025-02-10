721d1f893697c3d89f0a5fcbc31dd36aba50dd3a
Axel Hedman <axelhedman00@gmail.com>
2025-02-10 20:58:39 +0200

    home-manager

--- flake.nix
+++ flake.nix
@@ -1,22 +1,30 @@
 {
-
   description = "NixOS Setup";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; 
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
     let 
       lib = nixpkgs.lib;
+      system = "x86_64-linux"; 
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+	inherit system;
 	modules = [ ./configuration.nix ];
       };
     }; 
+    homeConfigurations = {
+      knappen = home-manager.lib.homeManagerConfiguration {
+	inherit pkgs;
+	modules = [ ./home.nix ];
+      };
+    }; 
   };
-
 }
 
