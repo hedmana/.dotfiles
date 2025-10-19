ddf4cb474ba14681549d2148fce17d7cd8307c66
Axel <axelhedman00@gmail.com>
2025-10-19 18:45:47 +0300

    Add Treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,12 @@
 -- Init lazy.nvi
 require("config.lazy")
 
+-- Enable/disable Ruby-based plugins
+vim.g.loaded_ruby_provider = 0
+
+-- Enable/disable Perl-based plugins
+vim.g.loaded_perl_provider = 0
+
 -- Line numbering
 vim.opt.number = true
 vim.opt.relativenumber = true
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "python", "bash", "html", "css", "javascript" }, 
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
