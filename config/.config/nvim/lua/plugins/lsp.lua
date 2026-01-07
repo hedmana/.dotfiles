48cb6abd818a120ab044838213261f7e7d2231f4
Axel Hedman <axel.hedman@gostalabs.com>
2026-01-07 15:04:31 +0200

    Fix language support and add autocomplete

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,97 @@
 return {
+  -- LSP Configuration
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp", -- Autocompletion capabilities for LSP
+    },
+    config = function()
+      require("mason").setup()
+      
+      require("mason-lspconfig").setup({
+        -- Ensure these servers are installed
+        ensure_installed = {
+          "pyright", -- Python
+          "ts_ls", -- JavaScript/TypeScript
+          "clangd", -- C++
+        },
+        -- Automatically setup servers
+        handlers = {
+            function(server_name)
+                local capabilities = require("cmp_nvim_lsp").default_capabilities()
+                require("lspconfig")[server_name].setup({
+                    capabilities = capabilities,
+                })
+            end,
+        }
+      })
+      
+      -- Diagnostic configuration
+      vim.diagnostic.config({
+        virtual_text = true,
+        float = {
+          source = "always",
+        },
+      })
 
+      -- Keymaps for diagnostics
+      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+    end,
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "windwp/nvim-autopairs", 
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      
+      -- If you want insert `(` after select function or method item
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+
+      -- Load friendly-snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
 }
--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "python", "bash", "html", "css", "javascript" }, 
+      ensure_installed = { "lua", "python", "bash", "html", "css", "javascript", "typescript", "tsx", "c", "cpp" }, 
       sync_install = false,
       auto_install = true,
       highlight = { enable = true },
